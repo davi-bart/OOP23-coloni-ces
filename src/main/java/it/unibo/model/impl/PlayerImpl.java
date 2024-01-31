8e87ddc2ebdb87e2e43d6120662806d01265625c
Andrea Samori <andrea.samori3@studio.unibo.it>
2024-01-31 19:26:04 +0100

    added Trader interface and changed PlayerImpl

--- src/main/java/it/unibo/model/impl/PlayerImpl.java
+++ src/main/java/it/unibo/model/impl/PlayerImpl.java
@@ -2,22 +2,26 @@ package it.unibo.model.impl;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import it.unibo.common.ResourceType;
 import it.unibo.model.api.Player;
+import it.unibo.model.api.Trader;
 
 /**
  * Implementation of Player.
  */
-public final class PlayerImpl implements Player {
+public final class PlayerImpl implements Player, Trader {
     private final Map<ResourceType, Integer> hand = new HashMap<>();
     private int victoryPoints;
+    private final String name;
 
     /**
      * Create a Player implementation that has the given name.
      * 
      */
-    public PlayerImpl() {
+    public PlayerImpl(final String name) {
+        this.name = name;
         victoryPoints = 0;
         for (final ResourceType resource : ResourceType.values()) {
             hand.put(resource, 0);
@@ -37,7 +41,7 @@ public final class PlayerImpl implements Player {
     public void addResources(final ResourceType resource, final int amount) {
         if (amount > 0) {
             hand.put(resource, hand.get(resource) + amount);
-        }else {
+        } else {
             throw new IllegalArgumentException("amount must be positive");
         }
     }
@@ -46,7 +50,7 @@ public final class PlayerImpl implements Player {
     public void removeResources(final ResourceType resource, final int amount) {
         if (hand.get(resource) >= amount) {
             hand.put(resource, hand.get(resource) - amount);
-        }else {
+        } else {
             throw new IllegalArgumentException("amount must be minor than the total resource");
         }
     }
@@ -59,5 +63,27 @@ public final class PlayerImpl implements Player {
     @Override
     public int getVictoryPoints() {
         return this.victoryPoints;
+    }
+
+    @Override
+    public void acceptTrade(final Map<ResourceType, Integer> givenResouces,
+            final Map<ResourceType, Integer> recivingResources) {
+
+        for (Entry<ResourceType, Integer> resource : givenResouces.entrySet()) {
+            removeResources(resource.getKey(), resource.getValue());
+        }
+        for (Entry<ResourceType, Integer> resource : recivingResources.entrySet()) {
+            addResources(resource.getKey(), resource.getValue());
+        }
+    }
+
+    @Override
+    public boolean canTrade(Map<ResourceType, Integer> givenResouces) {
+        for (Entry<ResourceType, Integer> resource : givenResouces.entrySet()) {
+            if (getResource(resource.getKey()) < resource.getValue()) {
+                return false;
+            }
+        }
+        return true;
     }
 }
