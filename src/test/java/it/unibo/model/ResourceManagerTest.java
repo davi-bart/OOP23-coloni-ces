ee56eda28ef1b1a03166e921587da7e680605f0b
Andrea Samori <andrea.samori3@studio.unibo.it>
2024-02-05 15:44:08 +0100

    added some tests in ResourceManagerTest

--- src/test/java/it/unibo/model/ResourceManagerTest.java
+++ src/test/java/it/unibo/model/ResourceManagerTest.java
@@ -1,7 +1,7 @@
 package it.unibo.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.HashMap;
 import java.util.List;
@@ -14,9 +14,22 @@ import it.unibo.common.ResourceType;
 import it.unibo.model.api.ResourceManager;
 import it.unibo.model.impl.ResourceManagerImpl;
 
-public class ResourceManagerTest {
-    private ResourceManager rm = new ResourceManagerImpl(List.of("Luca", "Andrea", "Alex", "Sbara", "Bank"));
+// CHECKSTYLE: MagicNumber OFF
+/**
+ * Test for the Resource Manager.
+ */
+class ResourceManagerTest {
 
+    private ResourceManager rm;
+
+    @BeforeEach
+    void init() {
+        this.rm = new ResourceManagerImpl(List.of("Luca", "Andrea", "Alex", "Sbara", "Bank"));
+    }
+
+    /**
+     * Test addResources.
+     */
     @Test
     public void testAddResources() {
         assertEquals(0, rm.getResource("luca", ResourceType.BRICK));
@@ -24,4 +37,44 @@ public class ResourceManagerTest {
         assertEquals(3, rm.getResource("luca", ResourceType.BRICK));
 
     }
+
+    /**
+     * Test removeResources.
+     */
+    @Test
+    public void testRemoveResources() {
+        assertEquals(0, rm.getResource("luca", ResourceType.BRICK));
+        rm.addResources("luca", ResourceType.BRICK, 5);
+        rm.removeResources("luca", ResourceType.BRICK, 3);
+        assertEquals(2, rm.getResource("luca", ResourceType.BRICK));
+
+    }
+
+    /**
+     * Test trade.
+     */
+    @Test
+    public void testTrade() {
+        final Map<ResourceType, Integer> givingResource = new HashMap<>();
+        final Map<ResourceType, Integer> recivingResource = new HashMap<>();
+
+        rm.addResources("luca", ResourceType.BRICK, 5);
+        rm.addResources("luca", ResourceType.WOOL, 5);
+        rm.addResources("luca", ResourceType.GRAIN, 5);
+        rm.addResources("alex", ResourceType.LUMBER, 5);
+        rm.addResources("alex", ResourceType.ORE, 5);
+        recivingResource.put(ResourceType.BRICK, 2);
+        recivingResource.put(ResourceType.WOOL, 3);
+        assertTrue(rm.canTrade("luca", recivingResource));
+        givingResource.put(ResourceType.LUMBER, 2);
+        givingResource.put(ResourceType.ORE, 4);
+        rm.acceptTrade("alex", "luca", givingResource, recivingResource);
+        assertEquals(3, rm.getResource("luca", ResourceType.BRICK));
+        assertEquals(2, rm.getResource("luca", ResourceType.WOOL));
+        assertEquals(3, rm.getResource("alex", ResourceType.LUMBER));
+        assertEquals(1, rm.getResource("alex", ResourceType.ORE));
+
+    }
+    // CHECKSTYLE: MagicNumber ON
+
 }
