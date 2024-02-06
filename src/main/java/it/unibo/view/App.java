b4ceaa429016e010434ff49a43db88c2f5db116b
davi-bart <davide30.bartoli@gmail.com>
2024-02-06 20:35:39 +0100

    remove JavaFXApp

--- src/main/java/it/unibo/view/App.java
+++ src/main/java/it/unibo/view/App.java
@@ -1,10 +1,31 @@
 package it.unibo.view;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * Application.
  */
-public final class App {
-    private App() {
+public class App extends Application {
+    /**
+     * Start.
+     * 
+     * @param stage stage
+     * @throws Exception exception
+     * @see Application
+     */
+    @Override
+    public void start(final Stage stage) throws Exception {
+        final AppView appView = new AppView();
+
+        final Scene scene = appView.getScene();
+
+        stage.setTitle("I Coloni di Cesena");
+        stage.setScene(scene);
+        stage.setMaximized(true);
+
+        stage.show();
     }
 
     /**
@@ -12,7 +33,23 @@ public final class App {
      * 
      * @param args
      */
-    public static void main(final String... args) {
-        JavaFXApp.run(args);
+    public static void run(final String... args) {
+        launch(args);
+    }
+
+    /**
+     * Entry point's class.
+     */
+    public static final class Main {
+        private Main() {
+        }
+
+        /**
+         * Program's entry point.
+         * @param args
+         */
+        public static void main(final String... args) {
+            App.run(args);
+        }
     }
 }
