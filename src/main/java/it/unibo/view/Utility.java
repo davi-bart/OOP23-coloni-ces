1983b3643eb7d0d97f9d1113384417418832324c
davi-bart <davide30.bartoli@gmail.com>
2024-02-07 23:33:27 +0100

    add otherRoard function in Utility

--- src/main/java/it/unibo/view/Utility.java
+++ src/main/java/it/unibo/view/Utility.java
@@ -6,6 +6,8 @@ import java.util.List;
 import java.util.Map;
 
 import it.unibo.common.RoadDirection;
+import it.unibo.common.TileCoordinates;
+import it.unibo.common.TileCoordinatesImpl;
 import javafx.util.Pair;
 
 /**
@@ -19,8 +21,8 @@ public final class Utility {
 
     // TODO: check if we want to use javafx pairs.
     /**
-     * returns the coordinates of a hexagon's vertices in counterclockwise order,
-     * starting from the topmost one.
+     * returns the coordinates of a hexagon's vertices in clockwise order,
+     * starting from the bottom one.
      * 
      * @param radius radius
      * @param x      x coordinate of the center
@@ -50,13 +52,40 @@ public final class Utility {
         final List<Pair<Double, Double>> hexagonCoordinates = getHexagonCoordinates(radius, x, y);
 
         // CHECKSTYLE: MagicNumber OFF
-        roads.put(RoadDirection.UPLEFT, new Pair<>(hexagonCoordinates.get(0), hexagonCoordinates.get(1)));
+        roads.put(RoadDirection.DOWNLEFT, new Pair<>(hexagonCoordinates.get(0), hexagonCoordinates.get(1)));
         roads.put(RoadDirection.LEFT, new Pair<>(hexagonCoordinates.get(1), hexagonCoordinates.get(2)));
-        roads.put(RoadDirection.DOWNLEFT, new Pair<>(hexagonCoordinates.get(2), hexagonCoordinates.get(3)));
-        roads.put(RoadDirection.DOWNRIGHT, new Pair<>(hexagonCoordinates.get(3), hexagonCoordinates.get(4)));
+        roads.put(RoadDirection.UPLEFT, new Pair<>(hexagonCoordinates.get(2), hexagonCoordinates.get(3)));
+        roads.put(RoadDirection.UPRIGHT, new Pair<>(hexagonCoordinates.get(3), hexagonCoordinates.get(4)));
         roads.put(RoadDirection.RIGHT, new Pair<>(hexagonCoordinates.get(4), hexagonCoordinates.get(5)));
-        roads.put(RoadDirection.UPRIGHT, new Pair<>(hexagonCoordinates.get(5), hexagonCoordinates.get(0)));
+        roads.put(RoadDirection.DOWNRIGHT, new Pair<>(hexagonCoordinates.get(5), hexagonCoordinates.get(0)));
         // CHECKSTYLE: MagicNumber ON
         return roads;
+    }
+
+    /**
+     * returns the coordinates of the equivalent road of a given road.
+     * 
+     * @param tile
+     * @param road
+     * @return the equivalent road
+     */
+    static Pair<TileCoordinates, RoadDirection> otherRoard(final TileCoordinates tile, final RoadDirection road) {
+        final int colShift = tile.getRow() % 2;
+        return switch (road) {
+            case UPLEFT ->
+                new Pair<>(new TileCoordinatesImpl(tile.getRow() - 1, tile.getCol() - 1 + colShift),
+                        RoadDirection.DOWNRIGHT);
+            case LEFT -> new Pair<>(new TileCoordinatesImpl(tile.getRow(), tile.getCol() - 1), RoadDirection.RIGHT);
+            case DOWNLEFT ->
+                new Pair<>(new TileCoordinatesImpl(tile.getRow() + 1, tile.getCol() - 1 + colShift),
+                        RoadDirection.UPRIGHT);
+            case DOWNRIGHT ->
+                new Pair<>(new TileCoordinatesImpl(tile.getRow() + 1, tile.getCol() + colShift), RoadDirection.UPLEFT);
+            case RIGHT -> new Pair<>(new TileCoordinatesImpl(tile.getRow(), tile.getCol() + 1), RoadDirection.LEFT);
+            case UPRIGHT ->
+                new Pair<>(new TileCoordinatesImpl(tile.getRow() - 1, tile.getCol() + colShift),
+                        RoadDirection.DOWNLEFT);
+            default -> throw new IllegalArgumentException("Invalid road direction");
+        };
     }
 }
