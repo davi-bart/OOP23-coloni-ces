b4ea48415ed7ae2fb1f20737012be10329cc62ad
Alex Guerrini <alex.guerrini@studio.unibo.it>
2024-02-14 16:16:05 +0100

    changed alert type in menu

--- src/main/java/it/unibo/view/Menu.java
+++ src/main/java/it/unibo/view/Menu.java
@@ -3,18 +3,15 @@ package it.unibo.view;
 import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.collections.ObservableListBase;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
-import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundImage;
@@ -23,7 +20,6 @@ import javafx.scene.layout.BackgroundRepeat;
 import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
-import javafx.stage.Popup;
 import javafx.stage.Stage;
 import java.io.IOException;
 
@@ -70,7 +66,7 @@ public class Menu {
         final Scene scene = new Scene(root);
         final TextField textField = new TextField();
         final TableView<String> tableView = new TableView<String>();
-        final Alert popUp = new Alert(AlertType.CONFIRMATION);
+        final Alert popUp = new Alert(AlertType.ERROR);
         final TableColumn<String, String> playerName = new TableColumn<String, String>("Player Name");
 
         playerName.setCellValueFactory(param -> new ReadOnlyStringWrapper(param.getValue()));
@@ -80,11 +76,7 @@ public class Menu {
         playButton.setOnMouseClicked(e -> {
             if (players.size() >= 1) {
                 stage.close();
-                try {
-                    new AppView(stage).draw();
-                } catch (IOException e1) {
-                    e1.printStackTrace();
-                }
+                new AppViewImpl(stage).draw();
             } else {
                 popUp.setHeaderText("You need atleast one player to start the game");
                 popUp.showAndWait();
