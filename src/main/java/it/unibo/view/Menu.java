76c2257f15e6652963d371af575706109db76501
Alex Guerrini <alex.guerrini@studio.unibo.it>
2024-02-14 16:11:08 +0100

    updated menu to add players to the game

--- src/main/java/it/unibo/view/Menu.java
+++ src/main/java/it/unibo/view/Menu.java
@@ -1,9 +1,20 @@
 package it.unibo.view;
 
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.ObservableListBase;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundImage;
@@ -12,16 +23,16 @@ import javafx.scene.layout.BackgroundRepeat;
 import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Popup;
 import javafx.stage.Stage;
 import java.io.IOException;
-import java.util.List;
 
 /**
  * Application.
  */
 public class Menu {
     private final Stage stage;
-    private final List<String> players = List.of("Alex", "Lucone", "Monaco", "Dave");
+    private final ObservableList<String> players = FXCollections.observableArrayList();
 
     /**
      * Constructor of Menu.
@@ -53,12 +64,48 @@ public class Menu {
      */
     public Scene getScene() throws IOException {
         final BorderPane root = FXMLLoader.load(ClassLoader.getSystemResource("layouts/main.fxml"));
-        final VBox box = new VBox();
+        final VBox playBox = new VBox();
         final Button playButton = new Button("PLAY");
+        final Button addButton = new Button("ADD PLAYER");
         final Scene scene = new Scene(root);
+        final TextField textField = new TextField();
+        final TableView<String> tableView = new TableView<String>();
+        final Alert popUp = new Alert(AlertType.CONFIRMATION);
+        final TableColumn<String, String> playerName = new TableColumn<String, String>("Player Name");
+
+        playerName.setCellValueFactory(param -> new ReadOnlyStringWrapper(param.getValue()));
+        tableView.getColumns().add(playerName);
+        tableView.setMaxSize(300, 200);
+        tableView.setMinSize(300, 0);
         playButton.setOnMouseClicked(e -> {
-            stage.close();
-            new AppViewImpl(stage).draw();
+            if (players.size() >= 1) {
+                stage.close();
+                try {
+                    new AppView(stage).draw();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            } else {
+                popUp.setHeaderText("You need atleast one player to start the game");
+                popUp.showAndWait();
+            }
+
+        });
+
+        addButton.setOnMouseClicked(e -> {
+            if (players.size() < 4) {
+                if (players.contains(textField.getText())) {
+                    popUp.setHeaderText("The chosen name is already taken");
+                    popUp.showAndWait();
+                } else {
+                    players.add(textField.getText());
+                    textField.clear();
+                }
+            } else {
+                popUp.setHeaderText("The player limit has been reached");
+                popUp.showAndWait();
+            }
+
         });
 
         Image image = new Image("imgs/menu/SettlersOfCesena.png");
@@ -67,11 +114,16 @@ public class Menu {
                 BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
         Background background = new Background(backgroundImage);
 
-        box.getChildren().add(playButton);
-        box.setAlignment(Pos.CENTER);
-        root.setBackground(background);
-        root.setCenter(box);
+        tableView.setItems(players);
+        playerName.prefWidthProperty().bind(tableView.widthProperty());
+        textField.setPromptText("Insert player names");
+        textField.setMaxSize(200, 200);
+        playBox.getChildren().addAll(textField, addButton, tableView, playButton);
+        playBox.setSpacing(5);
 
+        playBox.setAlignment(Pos.CENTER);
+        root.setBackground(background);
+        root.setCenter(playBox);
         return scene;
     }
 
