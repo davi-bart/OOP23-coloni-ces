ac872a513d690222c470c8bf8bd8091f42630845
davi-bart <davide30.bartoli@gmail.com>
2024-02-14 15:00:32 +0100

    move roadManager and PropertyManager from Board to BoardController

--- src/main/java/it/unibo/model/api/Board.java
+++ src/main/java/it/unibo/model/api/Board.java
@@ -2,11 +2,7 @@ package it.unibo.model.api;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
-import it.unibo.common.api.PropertyPosition;
-import it.unibo.common.api.PropertyType;
-import it.unibo.common.api.RoadPosition;
 import it.unibo.common.api.TerrainType;
 import it.unibo.common.api.TileCoordinates;
 
@@ -38,28 +34,6 @@ public interface Board {
     TerrainType getTileTerrainType(TileCoordinates pos);
 
     /**
-     * get the roads of a player.
-     * 
-     * @param player the player
-     * @return the list of roads
-     */
-    Set<Road> getPlayerRoads(Player player);
-
-    /**
-     * @param player
-     * @return a set of the roads built by {@code player}
-     */
-    Set<Property> getPlayerProperties(Player player);
-
-    /**
-     * get the property type of the property at the given position.
-     * 
-     * @param position the position of the property
-     * @return the type of the property
-     */
-    PropertyType getPropertyType(PropertyPosition position);
-
-    /**
      * @return the position of the robber
      */
     Optional<TileCoordinates> getRobberPosition();
@@ -71,35 +45,4 @@ public interface Board {
      * @param coordinates coordinates of the new robber's position
      */
     void setRobberPosition(TileCoordinates coordinates);
-
-    /**
-     * Build a settlement in the specified position.
-     * 
-     * @param position position
-     * @param player   owner of the settlement
-     */
-    void buildSettlement(PropertyPosition position, Player player);
-
-    /**
-     * Build a city in the specified position.
-     * 
-     * @param position position
-     * @param player   owner of the settlement
-     */
-    void buildCity(PropertyPosition position, Player player);
-
-    /**
-     * Builds the road at position {@code position}.
-     * 
-     * @param position position of the road
-     * @param player   owner of the road
-     */
-    void buildRoad(RoadPosition position, Player player);
-
-    /**
-     * @param player
-     * @return the longest road length of {@code player}
-     */
-    int getLongestRoadLength(Player player);
-
 }
