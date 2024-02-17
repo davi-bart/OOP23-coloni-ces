3612fa0d4c2145b5bd3aee3c16a1dfd05adaa66f
Andrea Samori <andrea.samori3@studio.unibo.it>
2024-02-17 11:47:56 +0100

    added javadoc

--- src/main/java/it/unibo/controller/main/message/Message.java
+++ src/main/java/it/unibo/controller/main/message/Message.java
@@ -1,5 +1,12 @@
 package it.unibo.controller.main.message;
 
+/**
+ * Interface of a message.
+ */
 public interface Message {
+    /**
+     * 
+     * @return the string of the message.
+     */
     String getMessage();
 }
--- src/main/java/it/unibo/controller/main/message/MessageFactory.java
+++ src/main/java/it/unibo/controller/main/message/MessageFactory.java
@@ -5,11 +5,35 @@ import it.unibo.common.property.PropertyType;
 import it.unibo.common.tile.ResourceType;
 
 public interface MessageFactory {
+    /**
+     * Create a message representing the resource and the amount.
+     * 
+     * @param resource resource
+     * @param amount   amount
+     * @return the message
+     */
     Message createResourceMessage(ResourceType resource, int amount);
 
+    /**
+     * Create a message representing the card.
+     * 
+     * @param card card.
+     * @return the message.
+     */
     Message createCardMessage(CardType card);
 
+    /**
+     * Create a message representing the property.
+     * 
+     * @param property property
+     * @return the message
+     */
     Message createBuildPropertyMessage(PropertyType property);
 
+    /**
+     * Create a message representing the road.
+     * 
+     * @return the message
+     */
     Message createBuildRoadMessage();
 }
